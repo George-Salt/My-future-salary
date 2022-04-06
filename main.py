46cbe2e3a82d0a8ddb8a4c741f016abc6e0f37da
George Salt <solanikg3@gmail.com>
2022-04-06 12:01:03 +0300

    Fix predict_rub_salary

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ SJ_TITLE = "SuperJob Moscow"
 def predict_rub_salary(salary_from=None, salary_to=None):
     if salary_from and salary_to:
         expected_salary = int(salary_to + salary_from) / 2
-    elif not salary_from:
+    elif salary_to:
         expected_salary = salary_to * 1.2
-    elif not salary_to:
+    elif salary_from:
         expected_salary = salary_from * 0.8
     return expected_salary
 
